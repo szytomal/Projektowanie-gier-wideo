cb75f884e817ac0da1e89d27685df8ea0d93509f
szytomal <szytomal@gmail.com>
2020-10-23 18:25:42 +0200

    Godot01
    Pierwsze ćwiczenia w Godocie

---
+++ Godot01/Node.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+var k = 21
+var w = 37
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print ("Hello world")
+	print (k); print (w)
+	print (k+w)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
